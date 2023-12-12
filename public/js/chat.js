1b0d6622fb73679bb8326e5130c92e8d1ce32911
trungdung70199 <trungdung70199@gmail.com>
2023-12-12 11:34:23 +0900

    update chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,19 @@
-
 console.log('hello')
 // Websocket URL
 const url = '';
 // Websocket connect
 var socket = io.connect(url);
 
+// chat message
+socket.on('chat_message', (data) => {
+    console.log(data);
+})
+
 function sendMessage() {
-    var message = document.getElementById("message").value;
+    var message = document.getElementById('message').value;
     console.log(message);
     // chat server send message
     socket.emit('chat_message', {
-        message: message
+        message: message,
     })
 }
