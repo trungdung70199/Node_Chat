e90c07e0663c261f562f7387d764d46f55fa4d94
trungdung70199 <trungdung70199@gmail.com>
2023-12-12 11:11:44 +0900

    update chat index

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,7 +1,11 @@
-(() => {
-    console.log('hello')
-    // Websocket URL
-    const url = '';
-    // Websocket connect
-    var socket = io.connect(url);
-})()
+
+console.log('hello')
+// Websocket URL
+const url = '';
+// Websocket connect
+var socket = io.connect(url);
+
+function sendMessage() {
+    var message = document.getElementById("message").value;
+    console.log(message);
+}
