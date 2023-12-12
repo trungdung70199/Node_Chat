036678281c7b57921946c49dafc1713b44cd942b
trungdung70199 <trungdung70199@gmail.com>
2023-12-12 11:57:05 +0900

    add read server chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,10 +1,35 @@
+// console.log('hello');
+// // Websocket URL
+// const url = '';
+// // Websocket connect
+// var socket = io.connect(url);
+
+// // chat message
+// socket.on('chat_message', (data) => {
+//     console.log(data);
+//     var p = document.createElement('p');
+//     p.innerHTML = data.message;
+//     document.getElementById('messageList').append(p);
+// })
+
+// function sendMessage() {
+//     var message = document.getElementById('message').value;
+//     console.log(message);
+//     // chat server send message
+//     socket.emit('chat_message', {
+//         message: message,
+//     })
+// }
+
+
 console.log('hello');
-// Websocket URL
+
+// WebsocketのURL
 const url = '';
-// Websocket connect
+// Websocketの接続
 var socket = io.connect(url);
 
-// chat message
+// chatメッセージの受信
 socket.on('chat_message', (data) => {
     console.log(data);
     var p = document.createElement('p');
@@ -15,7 +40,7 @@ socket.on('chat_message', (data) => {
 function sendMessage() {
     var message = document.getElementById('message').value;
     console.log(message);
-    // chat server send message
+    // chatサーバにメッセージを送信
     socket.emit('chat_message', {
         message: message,
     })
--- server.js
+++ server.js
@@ -1,43 +1,89 @@
-// call express module
+// // call express module
+// const express = require('express')
+// // call http createServer module 
+// const { createServer } = require('node:http');
+// // call Socket.io Sever module
+// const { Server } = require('socket.io');
+
+// // Make Express server
+// const app = express();
+// const server = createServer(app);
+
+// // Make Socket.io
+// const io = new Server(server);
+
+// // Call .env
+// const dotenv = require('dotenv');
+// dotenv.config();
+// const host = process.env.HOST
+// const port = process.env.PORT
+
+// // Server setting
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(__dirname + '/public'));
+
+// //Socket.io
+// io.on('connection', (socket) => {
+//     console.log('connected!!!')
+
+//     // chat message
+//     socket.on('chat_message', (data) => {
+//         console.log(socket.id);
+//         console.log(data);
+//         // add user socket id
+//         data.socket_id = socket.id;
+//         // send message to user
+//         io.emit('chat_message', data);
+//     })
+// })
+
+// // start server
+// server.listen(port, host, () => {
+//     console.log(`listening on http://${host}:${port}`);
+// })
+
+
+// express module 読み込み
 const express = require('express')
-// call http createServer module 
+// http の createServer module 読み込み
 const { createServer } = require('node:http');
-// call Socket.io Sever module
+// Socket.io の Sever module 読み込み
 const { Server } = require('socket.io');
 
-// Make Express server
+// Expressサーバー作成
 const app = express();
+// サーバの設定
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+
 const server = createServer(app);
 
-// Make Socket.io
+// Socket.io を作成
 const io = new Server(server);
 
-// Call .env
+// .envの読み込み
 const dotenv = require('dotenv');
 dotenv.config();
 const host = process.env.HOST
 const port = process.env.PORT
 
-// Server setting
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/public'));
-
-//Socket.io
+//Socket.ioの接続
 io.on('connection', (socket) => {
     console.log('connected!!!')
 
-    // chat message
+    //chatメッセージの受信
     socket.on('chat_message', (data) => {
         console.log(socket.id);
         console.log(data);
-        // add user socket id
+        //送信ユーザのSocketIDを追加
         data.socket_id = socket.id;
-        // send message to user
+        //接続しているユーザにメッセージを送信
         io.emit('chat_message', data);
     })
 })
 
-// start server
+
+//サーバ起動
 server.listen(port, host, () => {
     console.log(`listening on http://${host}:${port}`);
 })
